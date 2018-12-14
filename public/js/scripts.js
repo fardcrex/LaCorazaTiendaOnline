fe6db5e0a67453f9fd66cc328449d8096fd6fdf1
Jair Conislla <1632874@utp.edu.pe>
2018-12-14 00:59:41 -0500

    se termina footer

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,12 +8,13 @@ var _activeMenu = require('./modules/active-menu');
 // saludo()
 // despedida()
 (0, _activeMenu.activeMenu)();
-var showMenu = function showMenu(toggleId, id1, id2, id3, id4, navId) {
+var showMenu = function showMenu(toggleId, id1, id2, id3, id35, id4, navId) {
   var toggle = document.getElementById(toggleId),
       nav = document.getElementById(navId),
       headerItems = document.getElementById(id1),
       headerItem1 = document.getElementById(id2),
       headerItem2 = document.getElementById(id3),
+      headerItem3 = document.getElementById(id35),
       mainList = document.getElementById(id4);
   if (toggle && nav && headerItems) {
     toggle.addEventListener('click', function () {
@@ -23,12 +24,13 @@ var showMenu = function showMenu(toggleId, id1, id2, id3, id4, navId) {
       headerItems.classList.toggle('main-header__item3');
       headerItem1.classList.toggle('main-header__item3__active__first');
       headerItem2.classList.toggle('main-header__item3__active__second');
+      headerItem3.classList.toggle('main-header__item3__active__third');
       mainList.classList.toggle('main-menu-list__active');
     });
   }
   console.log('hi');
 };
-showMenu('boton-main', 'header-items', 'header-items-1', 'header-items-2', 'main-list', 'nav-main');
+showMenu('boton-main', 'header-items', 'header-items-1', 'header-items-2', "header-items-3", 'main-list', 'nav-main');
 
 },{"./modules/active-menu":2,"./modules/example":3}],2:[function(require,module,exports){
 'use strict';
--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,13 @@ import {activeMenu} from './modules/active-menu'
 // saludo()
 // despedida()
 activeMenu()
-const showMenu = (toggleId,id1,id2,id3,id4, navId ) => {
+const showMenu = (toggleId,id1,id2,id3,id35, id4, navId ) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId),
     headerItems= document.getElementById(id1),
     headerItem1= document.getElementById(id2),
     headerItem2= document.getElementById(id3),
+    headerItem3= document.getElementById(id35),
     mainList= document.getElementById(id4)
   if( toggle && nav && headerItems){
     toggle.addEventListener('click', ()=> {
@@ -19,9 +20,10 @@ const showMenu = (toggleId,id1,id2,id3,id4, navId ) => {
       headerItems.classList.toggle('main-header__item3')
       headerItem1.classList.toggle('main-header__item3__active__first')
       headerItem2.classList.toggle('main-header__item3__active__second')     
+      headerItem3.classList.toggle('main-header__item3__active__third')     
       mainList.classList.toggle('main-menu-list__active') 
       })
   }
   console.log('hi')
 }
-showMenu('boton-main','header-items','header-items-1','header-items-2','main-list','nav-main')
+showMenu('boton-main','header-items','header-items-1','header-items-2',"header-items-3",'main-list','nav-main')
